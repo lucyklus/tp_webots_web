28d74d2617c43dc9c2187ca61800ae927cecc657
Lucia Mandová <lucka.mandova@gmail.com>
2022-12-06 15:55:25 +0100

    🔧added vite builder with taliwindcss

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import '../css/styles.css'
+
+document.querySelector('#app').innerHTML = `
+    <div>
+        <h1 class="text-3xl font-bold underline text-yellow-400">
+            Hello world!
+        </h1>
+    </div>
+`
