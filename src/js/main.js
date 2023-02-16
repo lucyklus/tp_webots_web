e5166bb56535b0171dab0844eec2bfce3f9d1fa0
Lucia Mandová <lucka.mandova@gmail.com>
2023-02-16 21:10:30 +0100

    :sparkles: add scss and yarn

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-import '../css/styles.css'
+import '../css/styles.scss'
 
 document.querySelector('#app').innerHTML = `
     <div>
-        <h1 class="text-3xl font-bold underline text-yellow-400">
+        <h1 class="title">
             Hello world!
         </h1>
     </div>
