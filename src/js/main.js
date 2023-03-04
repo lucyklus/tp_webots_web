9fe1ce2d6c9cc6b3e43d1d9edde2993ea9aaf89e
Lucia Mandová <lucka.mandova@gmail.com>
2023-03-04 19:57:18 +0100

    :sparkles: add js to semester divs; add easter egg; stick nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,30 @@
-import '../css/styles.scss';
+import '../css/styles.css';
+import filesOfSemesters from '../config/documents.json';
 
 function viewSemesterRecords(semester) {
   const recordsDiv = document.getElementById(`${semester}Records`);
+  const files = filesOfSemesters[semester];
+
   const saveImg = document.createElement('img');
   saveImg.setAttribute('src', '/src/images/save.png');
   saveImg.classList.add('img-save');
-  if (semester === 'firstSemester') {
-    for (let i = 1; i < 12; i += 1) {
-      const record = document.createElement('div');
-      record.classList.add('flex', 'justify-center');
-      const recordText = document.createElement('p');
-      recordText.innerHTML += `Zápisnica č.${i}`;
-      recordText.classList.add('text-blue', 'font-roboto', 'text-2xl', 'font-bold', 'w-[170px]');
-      record.appendChild(recordText.cloneNode(true));
-      record.appendChild(saveImg.cloneNode(true));
-      recordsDiv.appendChild(record);
-    }
-  } else {
-    for (let i = 12; i < 20; i += 1) {
-      const record = document.createElement('div');
-      record.className = 'flex';
-      const recordText = document.createElement('p');
-      recordText.innerHTML += `Zápisnica č.${i}`;
-      recordText.classList.add('text-blue', 'font-roboto', 'text-2xl', 'font-bold', 'w-[170px]');
-      record.appendChild(recordText.cloneNode(true));
-      record.appendChild(saveImg.cloneNode(true));
-      recordsDiv.appendChild(record);
-    }
+  for (const file of files) {
+    const record = document.createElement('span');
+    record.classList.add('flex', 'justify-center');
+    const recordText = document.createElement('p');
+    recordText.innerHTML += file.name;
+    recordText.classList.add('text-blue', 'font-roboto', 'text-2xl', 'font-bold', 'w-[170px]');
+    record.appendChild(recordText.cloneNode(true));
+    const recordLink = document.createElement('a');
+    recordLink.setAttribute('href', file.src);
+    recordLink.setAttribute('target', '_blank');
+    recordLink.appendChild(saveImg.cloneNode(true));
+    record.appendChild(recordLink.cloneNode(true));
+    recordsDiv.appendChild(record);
   }
 }
 
 function onSemesterClick(semester) {
-  const firstSemesterClicked = document.getElementById('firstSemester').hasAttribute('clicked');
-  const secondSemesterClicked = document.getElementById('secondSemester').hasAttribute('clicked');
   const semesterTitle = document.getElementById(`${semester}Title`);
   const semesterArrow = document.getElementById(`${semester}Arrow`);
   const semesterDiv = document.getElementById(semester);
@@ -40,23 +32,13 @@ function onSemesterClick(semester) {
   if (semesterDiv.hasAttribute('clicked')) {
     semesterDiv.removeAttribute('clicked');
     semesterTitle.classList.remove('text-purple');
-    semesterArrow.classList.remove('text-purple');
+    semesterArrow.classList.remove('icon-purple');
     semesterArrow.classList.remove('rotate-180');
     document.getElementById(`${semester}Records`).innerHTML = '';
   } else {
-    if (firstSemesterClicked && secondSemesterClicked) {
-      document.getElementById('firstSemester').setAttribute('clicked', '');
-      document.getElementById('firstSemesterTitle').classList.add('text-purple');
-      document.getElementById('firstSemesterArrow').classList.add('text-purple');
-      document.getElementById('firstSemesterArrow').classList.add('rotate-180');
-      document.getElementById('secondSemester').setAttribute('clicked', '');
-      document.getElementById('secondSemesterTitle').classList.add('text-purple');
-      document.getElementById('secondSemesterArrow').classList.add('text-purple');
-      document.getElementById('secondSemesterArrow').classList.add('rotate-180');
-    }
     semesterDiv.setAttribute('clicked', '');
     semesterTitle.classList.add('text-purple');
-    semesterArrow.classList.add('text-purple');
+    semesterArrow.classList.add('icon-purple');
     semesterArrow.classList.add('rotate-180');
     viewSemesterRecords(semester);
   }
